317cc46e7422aa25e2a687b29813a32f7b5878c1
Xiao Fan <tommyfanzju@gmail.com>
2019-08-26 16:10:39 -0700

    revised model_test.py

--- model_test.py
+++ model_test.py
@@ -1,6 +1,5 @@
 from sklearn.externals import joblib
 from sklearn.svm import SVC
-import csv
 from scipy.stats import skew, kurtosis
 from wavread import wavread
 from matplotlib.pyplot import *
@@ -15,12 +14,20 @@ Name: Xiao Fan
 """
 
 if __name__ == "__main__":
-
-    X = []
-    with open("feature.csv","r") as csvfile:
-        reader = csv.reader(csvfile)
-        for line in reader:
-            line = list(map(float, line))
-            X.append(line)
+    path = ["test.wav"]
+    audio_clip = [1, 5]
+    for i, yi in enumerate(path):
+        wavdata, wavtime, samplerate = wavread(yi, audio_clip)
+        wavdata2, wavtime2 = NASE(wavdata, 0.02 * samplerate, samplerate, audio_clip[0])
+        peak, s1, s1_start, s1_end, s2, s2_start, s2_end = HSSeg(wavdata2, wavtime2)
+        s1_start = ((s1_start - audio_clip[0]) * samplerate).astype(int)
+        s1_end = ((s1_end - audio_clip[0]) * samplerate).astype(int)
+        s2_start = ((s2_start - audio_clip[0]) * samplerate).astype(int)
+        s2_end = ((s2_end - audio_clip[0]) * samplerate).astype(int)
+        feature_vector = feature_extraction(wavdata, samplerate, s1_start, s1_end, s2_start, s2_end)
     svm = joblib.load('train_model.pkl')
-    print(svm.predict([X[64]]))
+    result = svm.predict([feature_vector])
+    if result == 0:
+        print("Your heart sound is normal")
+    elif result == 1:
+        print("Your heart sound is abnormal")
